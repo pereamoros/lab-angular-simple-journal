1fc2d8261e272a04d6f37df9d94da677ed8b1968
Pere Amorós <amorospere@gmail.com>
2018-02-22 17:30:59 +0100

    Iteration 1

--- starter-code/server/app.js
+++ starter-code/server/app.js
@@ -1,56 +1,52 @@
 const express      = require('express');
 const path         = require('path');
-const favicon      = require('serve-favicon');
 const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
-const layouts      = require('express-ejs-layouts');
 const mongoose     = require('mongoose');
 const cors         = require('cors');
 
-mongoose.connect('mongodb://localhost/journal-development');
-
 const app = express();
 
+// DB setup
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/journal-development', {
+ keepAlive: true,
+ reconnectTries: Number.MAX_VALUE
+});
+
 app.use(cors());
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(layouts);
 
-const index = require('./routes/index');
+const index = require('./routes/api/journal-entries');
 app.use('/', index);
 
 app.all('/*', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
+
+// -- error handlers
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404).json({error: 'not found'});
 });
 
-// error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({error: 'unexpected'});
+  }
 });
 
 module.exports = app;
